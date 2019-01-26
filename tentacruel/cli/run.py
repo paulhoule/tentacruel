2d60e860d98894129945b028b758fe93d16eb587
paul <paul.houle@ontology2.com>
2019-01-25 23:33:02 -0500

    CLI can play and stop;  that's a good place to end for the night.

--- tentacruel/cli/run.py
+++ tentacruel/cli/run.py
@@ -1,4 +1,5 @@
 import asyncio
+import sys
 from logging import getLogger, StreamHandler
 from os import environ
 from pathlib import Path
@@ -19,18 +20,49 @@ RECEIVER_IP = parameters["server"]["ip"]
 players = parameters["players"]
 tracks = parameters["tracks"]
 
-async def application(that:HeosClientProtocol):
-    player = that[players[0]["pid"]]
-    song = tracks[0]
-    del song["key"]
-    await player.add_to_queue(
-        aid = 4,
-        **song
-    )
+class Application:
+    def __init__(self,argv):
+        self.argv = argv
+        self.commands = Application.Commands(self)
+
+    class Commands:
+        def __init__(self,parent):
+            self.parent = parent
+
+        def that(self):
+            return self.parent.that
+
+        async def play(self):
+            player = self.that()[players[0]["pid"]]
+            await player.set_play_state("play")
+
+        async def stop(self):
+            player = self.that()[players[0]["pid"]]
+            await player.set_play_state("stop")
+
+
+    async def run(self,that:HeosClientProtocol):
+        self.that = that
+        if len(self.argv)==1:
+            self.help()
+
+        command_name = self.argv[1]
+        command_action = getattr(self.commands,command_name)
+        await command_action()
+
+    def help(self):
+        """
+        Please enter the correct command string
+
+        :return:
+        """
+        help(self.help)
 
 loop = asyncio.get_event_loop()
+application=Application(sys.argv)
+
 coro = loop.create_connection(
-    lambda: HeosClientProtocol(loop,start_action=application),
+    lambda: HeosClientProtocol(loop,start_action=application.run),
     RECEIVER_IP, HEOS_PORT
 )
 
