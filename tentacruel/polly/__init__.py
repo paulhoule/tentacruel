9229dd6c059e22d2881ba30eed95a21008f73e7c
paul <paul.houle@ontology2.com>
2019-03-12 21:11:10 -0400

    workable voices for "defend"

--- tentacruel/polly/__init__.py
+++ tentacruel/polly/__init__.py
@@ -27,48 +27,38 @@ with open(Path.home() / ".tentacruel" / "config.yaml") as a_stream:
     config = yaml.load(a_stream)
 
 polly = client("polly", config["aws"])
-text = "Thank you"
-text_name = "ty"
 # response = polly.describe_voices()
 # voices = [voice["Id"] for voice in response["Voices"] if voice['LanguageCode'].startswith('en')]
 
-voices = ["Ivy"]
+clips = [
+    ("thank-you", "Thank You"),
+    ("bedroom-on", "Would you please turn on the leftmost light switch in the bedroom?"),
+    ("hallway-on", "Excuse me.  Would you please toggle the lights in the stairwell?"),
+    ("downstairs-on", "Excuse me.  Would you please pull the chain "
+                      "on the light at the bottom of the stairs?")
+]
 
-target = Path(r"C:\voices")
-for voice in voices:
-    response = polly.synthesize_speech(
+target = Path(r"E:\voices")
+def speak(text_name, text, voice="Ivy"):
+    ssml = "<speak>"
+    ssml += f"<break time='0.9s' />"
+    ssml += "<amazon:breath duration='x-long' volume='x-loud' />"
+    ssml += text
+    ssml += "</speak>"
+    print(ssml)
+
+    complete_response = polly.synthesize_speech(
         OutputFormat="mp3",
         VoiceId=voice,
-        Text=text
+        TextType="ssml",
+        Text=ssml
     )
+    complete_audio = complete_response["AudioStream"].read()
+    filename = f"{voice.lower()}-{text_name}"
+    complete_to = target / f"{filename}.mp3"
+    complete_to.write_bytes(complete_audio)
+    complete_file = MP3(complete_to)
+    print(complete_file.info.length)
 
-    audio = response["AudioStream"].read()
-    try:
-        write_to = target / (str(uuid4()) + ".mp3")
-        write_to.write_bytes(audio)
-        audio_file = MP3(write_to)
-        durations = list(map(lambda x: 1.0 + 0.2*x, range(0, 10)))
-        for duration in durations:
-            initial_pad = duration - audio_file.info.length
-            ssml = "<speak>"
-            ssml += f"<break time='{initial_pad:.2f}s' />"
-            ssml += "<amazon:breath duration='x-long' volume='x-loud' />"
-            ssml += text
-            ssml += "</speak>"
-            print(ssml)
-
-            complete_response = polly.synthesize_speech(
-                OutputFormat="mp3",
-                VoiceId=voice,
-                TextType="ssml",
-                Text=ssml
-            )
-            complete_audio = complete_response["AudioStream"].read()
-            filename = f"{voice.lower()}-{text_name}-{duration:.1f}"
-            complete_to = target / f"{filename}.mp3"
-            complete_to.write_bytes(complete_audio)
-            complete_file = MP3(complete_to)
-            print(complete_file.info.length)
-    finally:
-        if write_to.exists():
-            write_to.unlink()
+for clip in clips:
+    speak(*clip)
