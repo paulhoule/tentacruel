338e3ef0dee77a5e8ddb4469f4dec572734c3891
paul <paul.houle@ontology2.com>
2019-03-18 16:33:07 -0400

    ping with asyncio!

--- tentacruel/pinger/__init__.py
+++ tentacruel/pinger/__init__.py
@@ -5,8 +5,20 @@ Routine that uses ping for presence detection in the LAN
 
 import platform    # For getting the operating system name
 import subprocess  # For executing a shell command
+import sys
+from asyncio import run, WindowsProactorEventLoopPolicy, set_event_loop_policy
+from asyncio.subprocess import PIPE, STDOUT, create_subprocess_exec
+from logging import getLogger, StreamHandler
+from os import environ
+from pathlib import Path
 
-def ping(host):
+import yaml
+from arango import aql
+
+# pylint: disable=invalid-name
+logger = getLogger(__name__)
+
+async def ping(host):
     """
     Returns True if host (str) responds to a ping request.
     Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
@@ -18,8 +30,66 @@ def ping(host):
     # Building the command. Ex: "ping -c 1 google.com"
     command = ['ping', param, '1', host]
 
-    result = subprocess.run(command, capture_output=True)
-    if result.returncode:
+    process = await create_subprocess_exec(*command, stdout=PIPE, stderr=STDOUT)
+    (stdout, _) = await process.communicate()
+    logger.debug("Pinged %s and got message %s", host, stdout)
+    logger.debug("Pinged %s and got returncode %s", host, process.returncode)
+    if process.returncode:
         return False
 
-    return result.stdout.find(b"unreachable.") == -1
+    return stdout.find(b"unreachable.") == -1
+
+
+def connect_to_adb(config):
+    """
+    Connect to arango database
+
+    :param config:
+    :return:
+    """
+    from arango import ArangoClient
+    client = ArangoClient(**config["arangodb"]["events"]["client"])
+    return client.db(**config["arangodb"]["events"]["database"])
+
+async def ping_all():
+    """
+    Ping every host on our list
+
+    :return:
+    """
+
+    if "LOGGING_LEVEL" in environ:
+        getLogger(None).setLevel(environ["LOGGING_LEVEL"])
+
+    getLogger(None).addHandler(StreamHandler())
+    with open(Path.home() / ".tentacruel" / "config.yaml") as a_stream:
+        config = yaml.load(a_stream)
+
+    adb = connect_to_adb(config)
+    cursor = adb.aql.execute("""
+        FOR row in pingables
+           FILTER NOT row.disabled
+           return row._key
+    """)
+    targets = [key for key in cursor]
+    for target in targets:
+        logger.debug("Pinging %s", target)
+        responded = await ping(target)
+        print(f"{target} {responded}")
+
+
+def ensure_proactor():
+    """
+    On Windows we get the selector event loop by default,  which does quite a few things,
+    but it doesn't do I/O to subprocesses.  Therefore,  this routine will select the
+    proactor event loop if we are running on Windows.
+
+    :return: None
+    """
+    if sys.platform == 'win32':
+        set_event_loop_policy(WindowsProactorEventLoopPolicy())
+
+
+if __name__ == "__main__":
+    ensure_proactor()
+    run(ping_all())
--- tests/test_pinger.py
+++ tests/test_pinger.py
@@ -1,7 +1,10 @@
-from tentacruel.pinger import ping
+from asyncio import run
+from tentacruel.pinger import ping, ensure_proactor
+
+ensure_proactor()
 
 def test_ping_localhost():
-    assert(ping("127.0.0.1"))
+    assert(run(ping("127.0.0.1")))
 
 def test_ping_elsewhere():
     """
@@ -12,7 +15,7 @@ def test_ping_elsewhere():
 
     :return:
     """
-    assert(not ping("10.254.254.254"))
+    assert(not run(ping("10.254.254.254")))
 
 if __name__ == "__main__":
     import pytest
