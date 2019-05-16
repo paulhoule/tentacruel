5c877a0dddba82cb9fb58edd51e1780e1f1505d1
paul <paul.houle@ontology2.com>
2019-05-15 21:26:14 -0400

    update web application to function as a web service that polls the SQS queue and tells us how many messages are waiting.

--- tentacruel/web/run.py
+++ tentacruel/web/run.py
@@ -1,18 +1,63 @@
-# pylint: disable=missing-docstring
+"""
+Run module for TC web application.
+
+"""
+import json
 
 from aiohttp.web import Application, Response, run_app, get
 from tentacruel import keep
 from tentacruel.config import get_config
+from tentacruel.sqs import SqsQueue
 
-async def hello(_):
-    return Response(text="Hello, world")
+class WebApp():
+    """
+    This design is slightly confused but to avoid any baggage involving
+    initialization it does not inherit from the Application class in aiohttp.web
+    """
 
-def main():
+    def __init__(self, config):
+        """
+        Constructor
+
+        :param config: application configuration dictionary
+        """
+        self.sqs = SqsQueue(config)
+        web_config = config["web"]
+        self.prefix = web_config["prefix"]
+
+    def attach_to(self, app: Application) -> None:
+        """
+        Attach this fully initialzed application to the ``Application`` object so we
+        can run
+
+        :param app:
+        :return:
+        """
+        app.add_routes([get(self.prefix+"sqs/count/", self.sqs_count)])
+
+    async def sqs_count(self, _) -> Response:
+        """
+        Return the message count in the SQS queue
+
+        :param _: Request Parameter that we ignore because we do the same regardless
+        :return: the Response
+        """
+        message = {
+            "message_count": self.sqs.count()
+        }
+        return Response(text=json.dumps(message))
+
+def main() -> None:
+    """
+    Main method of application
+
+    :return: nothing
+    """
     config = get_config()
     app = Application()
     web_config = config["web"]
-    prefix = web_config["prefix"]
-    app.add_routes([get(prefix, hello)])
+    webapp = WebApp(config)
+    webapp.attach_to(app)
 
     run_config = keep(web_config, {"host", "port"})
     run_app(app, **run_config)
